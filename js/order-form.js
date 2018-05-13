b1281eefbabf8320df1e0f1849f967b6db5b9137
Edward <edpantzar@gmail.com>
2018-05-13 14:21:34 -0500

    Added interactive order list

--- js/order-form.js
+++ js/order-form.js
@@ -19,28 +19,62 @@ $('document').ready(function(){
     }
   }
 
-  const getSelectionPrice = function() {
-    let option = $('.add-option-btn').val();
-    console.log(menu[option].price);
+  const order = {
+    subtotal: 0,
+    taxRate: 0.08,
+    tax: 0,
+    total: 0
   }
 
-  const getSelectionDescription = function() {
-    let option = $('.add-option-btn').val();
-    console.log(menu[option].description);
+  const getSelectionPrice = function(selection) {
+    return (menu[selection].price);
   }
 
-  const addSelectionToOrder = function() {
-    $('#order-table').find('.last-added').parent().append('<')
+  const getSelectionDescription = function(selection) {
+    return (menu[selection].description);
+  }
+
+  const addSelectionToOrder = function(selection) {
+    let name = getSelectionDescription(selection);
+    let price = getSelectionPrice(selection);
+    $('#item-name').find('ul').append('<li>'+name+'</li>');
+    $('#item-price').find('ul').append('<li>'+price+'</li>');
+  }
+
+  const roundPrice = function(number, precision) {
+    let factor = Math.pow(10, precision)
+    return Math.round(number * factor)/factor;
+  }
+
+  const updateSubtotal = function(price) {
+    return order.subtotal + price;
+  }
+
+  const updateTax = function(price) {
+    return (order.taxRate * price) + order.tax;
+  }
+
+  const updateTotal = function() {
+    return order.tax + order.subtotal;
+  }
+
+  const updateOrderTotalFields = function(selection) {
+    let price = getSelectionPrice(selection);
+    order.subtotal = updateSubtotal(price);
+    order.tax = updateTax(price);
+    order.total = updateTotal();
+    $('#subtotal').text('$' + roundPrice(order.subtotal, 2));
+    $('#tax').text('$' + roundPrice(order.tax, 2));
+    $('#total').text('$' + roundPrice(order.total, 2));
   }
 
   const acceptOrders = function() {
     $('.add-option-btn').click(function(){
       event.preventDefault();
-      getSelectionPrice();
-      getSelectionDescription();
+      addSelectionToOrder($(this).val());
+      updateOrderTotalFields($(this).val())
+      console.log(order);
     });
   }
-
   acceptOrders();
-
 });
